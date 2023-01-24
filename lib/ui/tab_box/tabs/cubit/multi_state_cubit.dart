71ebe84857e5bb4e022c7238a86d52e01c79959f
Samandar <samandarahadjonov@gmail.com>
2023-01-24 15:36:09 +0500

    multi state cubit logic done

--- lib/ui/tab_box/tabs/cubit/multi_state_cubit.dart
+++ lib/ui/tab_box/tabs/cubit/multi_state_cubit.dart
@@ -1,4 +1,6 @@
+import 'package:bloc_task/state_managers/cubit/multi_state_cubit/multi_state_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MultiStateCubitPage extends StatelessWidget {
   const MultiStateCubitPage({Key? key}) : super(key: key);
@@ -7,6 +9,28 @@ class MultiStateCubitPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Multi State Cubit")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.read<MultiStateCubit>().getData();
+
+        },
+      ),
+      body: Center(
+        child: BlocBuilder<MultiStateCubit,MultiStateState>(
+          builder: (context, state) {
+            if(state is GettingDataInProgress){
+              return Text("Loading");
+            }
+            if(state is GettingDataInSuccess){
+              return Text("Data keldi");
+            }
+            if(state is GettingDataInFailury){
+              return Text("Error");
+            }
+            return Container();
+          },
+        ),
+      ),
 
     );
   }
