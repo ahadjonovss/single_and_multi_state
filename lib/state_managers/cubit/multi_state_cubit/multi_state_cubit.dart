053b8bee44168b0ce04e4f87e1ca122ab5712923
Samandar <samandarahadjonov@gmail.com>
2023-01-24 15:31:46 +0500

    multi state cubit done

--- lib/state_managers/cubit/multi_state_cubit/multi_state_cubit.dart
+++ lib/state_managers/cubit/multi_state_cubit/multi_state_cubit.dart
@@ -1,8 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:bloc_task/data/services/api_service/api_service.dart';
+import 'package:bloc_task/utils/my_locale.dart';
 import 'package:meta/meta.dart';
+
+import '../../../data/models/my_response.dart';
 
 part 'multi_state_state.dart';
 
 class MultiStateCubit extends Cubit<MultiStateState> {
   MultiStateCubit() : super(MultiStateInitial());
+
+  getData() async {
+    emit(GettingDataInProgress());
+    MyResponse myResponse = await myLocale<ApiService>().getAllCards();
+    if(myResponse.error==""){
+      emit(GettingDataInSuccess(cards: myResponse.data));
+    }else{
+      emit(GettingDataInFailury(error: myResponse.error));
+    }
+  }
 }
