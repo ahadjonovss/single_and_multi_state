3fa5b18a777805a6aac4b5c5842cd5ab40d99b0d
Samandar <samandarahadjonov@gmail.com>
2023-01-24 15:07:19 +0500

    equatable event done

--- lib/state_managers/bloc/single_state_bloc/single_state_event.dart
+++ lib/state_managers/bloc/single_state_bloc/single_state_event.dart
@@ -2,3 +2,19 @@ part of 'single_state_bloc.dart';
 
 @immutable
 abstract class SingleStateEvent {}
+
+class ChangeStatusEvent extends SingleStateEvent{
+  final Status status;
+  ChangeStatusEvent({required this.status});
+}
+
+class SetCardsEvent extends SingleStateEvent{
+  final List cards;
+  SetCardsEvent({required this.cards});
+}
+
+class SetErrorEvent extends SingleStateEvent{
+  final String error;
+  SetErrorEvent({required this.error});
+}
+
