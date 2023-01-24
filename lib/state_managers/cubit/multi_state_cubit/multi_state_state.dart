d4a32a35dd326a6d051ca9b235e014dabdae354d
Samandar <samandarahadjonov@gmail.com>
2023-01-24 15:30:01 +0500

    multi state cubit done

--- lib/state_managers/cubit/multi_state_cubit/multi_state_state.dart
+++ lib/state_managers/cubit/multi_state_cubit/multi_state_state.dart
@@ -4,3 +4,18 @@ part of 'multi_state_cubit.dart';
 abstract class MultiStateState {}
 
 class MultiStateInitial extends MultiStateState {}
+
+class GettingDataInProgress extends MultiStateState{}
+class GettingDataInFailury extends MultiStateState{
+  String error;
+
+  GettingDataInFailury({required this.error});
+
+}
+class GettingDataInSuccess extends MultiStateState{
+  List cards;
+
+  GettingDataInSuccess({required this.cards});
+
+}
+
